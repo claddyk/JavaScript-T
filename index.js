a044fd8752cdbc14c392213bb8baf33c8d7941da
Mohit Kumar <madscientists1523@gmail.com>
2023-06-11 19:39:07 +0530

    TimeCounter

---
+++ index.js
@@ -0,0 +1,13 @@
+var counter = 1;
+var counter2=1;
+function printCounter() {
+    console.clear()
+    console.log(counter);
+    counter = counter +1
+}
+setInterval(printCounter, 1000)
+
+for (var i=0; i<10000; i++) {
+    counter2 = counter2+1;
+}
+console.log(counter2)
